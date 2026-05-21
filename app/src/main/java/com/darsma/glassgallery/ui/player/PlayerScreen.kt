653e53ca2548ba73a1b0ed13aac2bded71471b38
DarsmaOfficial <darsmaofficial@gmail.com>
2026-05-21 18:06:52 +0000

    Fix: togetherWith infix usage in play-pause AnimatedContent

--- app/src/main/java/com/darsma/glassgallery/ui/player/PlayerScreen.kt
+++ app/src/main/java/com/darsma/glassgallery/ui/player/PlayerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
@@ -285,9 +286,11 @@ private fun PlayPauseButton(
         androidx.compose.animation.AnimatedContent(
             targetState   = isPlaying,
             transitionSpec = {
-                (fadeIn(spring(stiffness = Spring.StiffnessMedium)) +
-                    scaleIn(initialScale = 0.7f, animationSpec = spring(dampingRatio = 0.55f)))
-                    .togetherWith(fadeOut(spring(stiffness = Spring.StiffnessMedium)))
+                val enter = fadeIn(spring(stiffness = Spring.StiffnessMedium)) +
+                    scaleIn(initialScale = 0.7f, animationSpec = spring(dampingRatio = 0.55f))
+                val exit = fadeOut(spring(stiffness = Spring.StiffnessMedium)) +
+                    scaleOut(targetScale = 0.7f, animationSpec = spring(dampingRatio = 0.55f))
+                enter togetherWith exit
             },
             label = "play-pause-icon",
         ) { playing ->

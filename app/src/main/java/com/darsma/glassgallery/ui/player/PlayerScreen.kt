8c97f353cf579a931ec383474ec8c053747eafbf
DarsmaOfficial <darsmaofficial@gmail.com>
2026-05-21 18:06:34 +0000

    Fix: add togetherWith import

--- app/src/main/java/com/darsma/glassgallery/ui/player/PlayerScreen.kt
+++ app/src/main/java/com/darsma/glassgallery/ui/player/PlayerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
+import androidx.compose.animation.togetherWith
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
